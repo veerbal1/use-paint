a7b583a99327440ee2ef0a06ec6457b4fb3cf73b
Veerbal Singh <Veerbalsingh1@gmail.com>
2024-02-10 11:22:16 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-blue-100 flex justify-center items-center">
+      <canvas className="bg-white"></canvas>
+    </div>
+  );
+};
+
+export default App;
