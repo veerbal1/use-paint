994e4bdb19c17c271832d58e3c50c28eb4771c97
Veerbal Singh <Veerbalsingh1@gmail.com>
2024-02-10 11:37:32 +0530

    Created basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 const App = () => {
   return (
-    <div className="w-screen h-screen bg-blue-100 flex justify-center items-center">
-      <canvas className="bg-white"></canvas>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-[60%] flex flex-col items-center">
+        <canvas className="rounded-3xl w-full shadow-lg border border-gray-200"></canvas>
+        <div className="flex w-full justify-between items-center mt-4 min-h-16 rounded-full shadow-lg"></div>
+      </div>
     </div>
   );
 };
